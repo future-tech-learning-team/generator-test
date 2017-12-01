f8e7fd549eeb652963ed456720b0af60e60d4b9b
shiyunjie <shiyunjie_nw@163.com>
2017-12-01 15:24:11 +0800

    chore：初始化项目时要求命名项目名称，选择pc或App两种模版

--- app/index.js
+++ app/index.js
@@ -2,18 +2,58 @@
  * Created by shiyunjie on 17/11/21.
  */
 const Generator = require('yeoman-generator');
-
+const fs = require('fs');
 module.exports = class extends Generator {
+
 
   constructor(args, opts) {
     super(args, opts);
 
     // 添加你自己的option
-    this.option('inputname', {
-      type: String,
-      desc: 'your app name',
-      defaults: 'test-app'
-    }); // This method adds support for a `--babel` flag
+    /*  this.option('inputName', {
+     type: String,
+     desc: 'your app name',
+     defaults: 'test-app'
+     }); // This method adds support for a `--inputname` flag
+     */
+    this.projectType = {}
+    this.appName = 'myApp'
+
+  }
+  prompting() {
+  /*  return this.prompt([{
+        type    : 'input',
+        name    : 'name',
+        message : 'Your project name',
+        default : this.appname // Default to current folder name
+      }, {
+        type    : 'confirm',
+        name    : 'cool',
+        message : 'Would you like to enable the Cool feature?'
+      }]).then((answers) => {
+        this.log('app name', answers.name);
+    this.log('cool feature', answers.cool);
+  });*/
+
+    return this.prompt([ {
+      type: 'input',
+      name: 'appname',
+      message: 'name your project',
+      default : this.appName
+    },
+      {
+        type: 'list',
+        name: 'projectType',
+        message: 'which type do you need?',
+        choices: ['PC', 'App'],
+        filter: function(val) {
+          return val.toLowerCase();
+        }
+      }]).then((answers) => {
+        this.projectType = answers.projectType
+        this.appName = answers.appname
+        this.log(this.projectType,this.appName);
+  });
   }
 
   method1() {
@@ -26,12 +66,28 @@ module.exports = class extends Generator {
   }
 
   writing(){
-    this._private_copyFile('test.txt',{title: 'Templating with Yeoman test.txt' });
-    this._private_copyFile('package.json',{appname: this.options.inputname});
+    switch (this.projectType){
+      case 'pc':
+        this._private_copyFile('pc.txt',{title: 'Templating with Yeoman pc.txt' });
+        break;
+      case 'app':
+        this._private_copyFile('app.txt',{title: 'Templating with Yeoman app.txt' });
+        break;
+      default:
+        break;
+    }
+
+    //this._private_copyFile('package.json',{appname: this.options.inputname});
+    this._private_copyFile('package.json',{appname: this.appName});
     this._private_copyFile('.babelrc',{}); // Babel 转码配置
     this._private_copyFile('./scripts/babelSrc.sh',{});
+    this._private_copyFile('./scripts/babelTra.js',{});
     //this._private_copyFile('./build/',{});
     this._private_copyFile('./src/index.js',{});
+    this._private_copyFile('./src/pages/',{}); // 先建目录
+    this._private_copyFile('./src/pages/home.js',{});
+
+
   }
 
   _private_copyFile(fileName,template) { // 私有方法，不会自动执行
@@ -46,6 +102,7 @@ module.exports = class extends Generator {
     );
 
   }
+
 
   // 方法名称无所谓,脚手架会按顺序同步执行  method1，method2 。。。
 };
