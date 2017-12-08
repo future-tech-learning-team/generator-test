fba2a651971b5b7155c5ed0ad7bd168081b2ee0a
shiyunjie <shiyunjie_nw@163.com>
2017-12-08 17:05:03 +0800

    fix: 修改babel-core不使用配置文件

--- scripts/gitTest.js
+++ scripts/gitTest.js
@@ -31,7 +31,7 @@ const main = async () => {
     const version = semver.inc(packageJS.version, 'prerelease', 'beta');
     console.log('非master分支，只能提交beta版本');
 
-    await execa.shell(`npm version ${version}`);
+    await execa.shell(`npm version ${version} && npm publish`);
     console.log('已修改版本号为:', version);
   }
 
