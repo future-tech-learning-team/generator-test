730c89cf398ea67f73f00f2c8d39e1c098053506
chenxuhua <chenxvhua@mobanker.com>
2017-12-11 11:15:14 +0800

    feat(generator-test):完成第二阶段内容

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,18 +7,44 @@ const package = require("../package.json");   // 获取package.json
 const branch = require('git-branch');
 const branchName=branch.sync();
 console.log(branchName);
+const semver = require('semver');
 
-//x.x.x-beta.x
-//0.0.0-beta.1
+var newVersion=semver.inc('1.2.3', 'patch')
+console.log("str=",newVersion);
+
+// major(v): Return the major version number.
+// minor(v): Return the minor version number.
+// patch(v): Return the patch version number.
+
 
 var versionTest=/^\d\.\d\.\d-beta\.\d$/;
-if(branchName !== 'master' && !versionTest.test(package.version)){
-    console.log('非master分支，只能提交beta版本',"branchName",branchName,"version",package.version);
+if(branchName !== 'master'){
+    var newVersion=semver.inc(package.version, 'prerelease', 'beta')
+    console.log("预发布版本version=",package.version);
 }
 else{
-    execa.shell("npm publish")
-    console.log("发布成功")
+    var major=semver.major(package.version);
+    var minor=semver.minor(package.version);
+    var patch=semver.patch(package.version);
+    inquirer.prompt([{
+        type    : 'list',
+        name    : 'publishVersion',
+        message : '请选择发布版本',
+        choices: [(major+1)+"."+minor+"."+patch,major+"."+(minor+1)+"."+patch,major+"."+minor+"."+(patch+1)],
+        default : major+"."+minor+"."+(patch+1)
+    }]).then((answers) => {
+        console.log('选择发布版本', answers.publishVersion);
+        execa.shell("npm version "+answers.publishVersion+" && git  push —follow-tags")
+        // this.projectType = answers.projectType;
+    });
 }
+
+execa.shell("npm publish")
+console.log("发布成功")
+
+var inquirer = require('inquirer');
+
+
 
 
 
