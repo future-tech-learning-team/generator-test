3f56dd5fb72f3e0fe46fb60ffa0c650f6f172239
chenxuhua <chenxvhua@mobanker.com>
2017-12-28 10:59:54 +0800

    feat(generator-test):完成第三阶段内容

--- app/templates/src/scripts/build/index.js
+++ app/templates/src/scripts/build/index.js
@@ -14,7 +14,7 @@ function transcoder() {
         absolute: false
     }, function (er, files) {
         files.forEach(function (file) {
-            const result = babel.transformFileSync(path.resolve(pathStr, file))
+            const result = babel.transformFileSync(path.resolve(sourcePath, file))
             fse.outputFileSync(path.resolve(destinationPath, file), result.code, 'utf8')
         })
     })
