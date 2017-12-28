3a8808015d1f325f205f0eff4b744e1f7851f441
chenxuhua <chenxvhua@mobanker.com>
2017-12-28 18:47:20 +0800

    feat(generator-test):完成第三阶段内容

--- app/templates/src/scripts/release/index.js
+++ app/templates/src/scripts/release/index.js
@@ -2,7 +2,6 @@
  * Created by chenxuhua on 2017/12/8.
  */
 const execa = require('execa');
-const packageObj = require("../../../package.json");
 const branch = require('git-branch');
 const branchName = branch.sync();
 const semver = require('semver');
@@ -17,32 +16,51 @@ if (detectionFileStatus.stdout) {
 }
 
 if (branchName !== 'master') {
+    let packageObj = require("../../../package.json");
     const newVersion = semver.inc(packageObj.version, 'prerelease', 'beta')
     packageObj.version = newVersion;
     console.log("newVersion=", newVersion);
     fs.writeFileSync("package.json", JSON.stringify(packageObj, null, 2), "utf8");
+    packageObj.devDependencies={};
+    packageObj.scripts={};
+    fs.writeFileSync("build/package.json", JSON.stringify(packageObj, null, 2), "utf8");
     execa.shellSync('git add *');
     execa.shellSync('git commit -m "' + packageObj.version + '"');
     execa.shellSync('git push');
     console.log("非master分支执行完成");
+    execa.shellSync('cd build');
     execa.shellSync("npm publish")
     console.log("发布成功")
 }
 else {
-    const major = semver.major(packageObj.version);
-    const minor = semver.minor(packageObj.version);
-    const patch = semver.patch(packageObj.version);
     inquirer.prompt([{
         type: 'list',
         name: 'publishVersion',
         message: '请选择发布版本',
-        choices: [(major + 1) + "." + minor + "." + patch, major + "." + (minor + 1) + "." + patch, major + "." + minor + "." + (patch + 1)],
-        default: major + "." + minor + "." + (patch + 1)
+        choices: ["主版本号","次版本号","修订号"],
+        default:"主版本号"
     }]).then((answers) => {
-        console.log('选择发布版本', answers.publishVersion);
-        execa.shellSync("npm version " + answers.publishVersion)
+        let tag="premajor"
+        if(answers==="次版本号"){
+            tag="preminor"
+        }
+        else if(answers==="修订号"){
+            tag="prepatch"
+        }
+        //major, minor, patch  premajor  preminor prepatch
+        let packageObj = require("../../../package.json");
+        const newVersion = semver.inc(packageObj.version, tag)
+        console.log('你选择的发布版本', answers.publishVersion);
+        execa.shellSync("npm version " + answers.publishVersion);
+
+        packageObj.devDependencies={};
+        packageObj.scripts={};
+        fs.writeFileSync("build/package.json", JSON.stringify(packageObj, null, 2), "utf8");
+        execa.shellSync('git add *');
+        execa.shellSync('git commit -m "' + packageObj.version + '"');
         execa.shellSync("git  push --follow-tags")
         console.log("master分支执行完成");
+        execa.shellSync('cd build');
         execa.shellSync("npm publish")
         console.log("发布成功")
     });
