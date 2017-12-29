26f29743c0ab58c5a65ead4a2b2fb069f6a438ef
chenxuhua <chenxvhua@mobanker.com>
2017-12-29 11:18:37 +0800

    feat(generator-test):完成第三阶段内容

--- app/index.js
+++ app/index.js
@@ -5,48 +5,49 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
     prompting() {
         return this.prompt([{
-            type    : 'list',
-            name    : 'projectType',
-            message : '选择项目类型',
+            type: 'list',
+            name: 'projectType',
+            message: '选择项目类型',
             choices: ['pc', 'mobile'],
-            default : "pc"
+            default: "pc"
         }]).then((answers) => {
             this.log('你所选择的项目类型是', answers.projectType);
             this.projectType = answers.projectType;
         });
     }
-  writing() {
 
-    this.fs.copy(
-         this.templatePath('src'),
-         this.destinationPath('src')
-    );
+    writing() {
 
-    this.fs.copy(
-         this.templatePath('.babelrc'),
-         this.destinationPath('.babelrc')
-    );
-      this.fs.copy(
-          this.templatePath('.eslintignore'),
-          this.destinationPath('.eslintignore')
-      );
-      this.fs.copy(
-          this.templatePath('.gitignore'),
-          this.destinationPath('.gitignore')
-      );
-      this.fs.copy(
-          this.templatePath('package.json'),
-          this.destinationPath('package.json')
-      );
-     this.fs.copyTpl(
-          this.templatePath('index.html'),
-          this.destinationPath("index.html"),
-          {projectType:this.projectType}
-     );
+        this.fs.copy(
+            this.templatePath('src'),
+            this.destinationPath('src')
+        );
 
-  }
+        this.fs.copy(
+            this.templatePath('.babelrc'),
+            this.destinationPath('.babelrc')
+        );
+        this.fs.copy(
+            this.templatePath('.eslintignore'),
+            this.destinationPath('.eslintignore')
+        );
+        this.fs.copy(
+            this.templatePath('.gitignore'),
+            this.destinationPath('.gitignore')
+        );
+        this.fs.copy(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json')
+        );
+        this.fs.copyTpl(
+            this.templatePath('index.html'),
+            this.destinationPath("index.html"),
+            {projectType: this.projectType}
+        );
 
-  install() {
-    this.yarnInstall();
-  }
+    }
+
+    install() {
+        this.yarnInstall();
+    }
 };
