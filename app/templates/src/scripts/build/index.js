739005034b98e6182a30ac1678027d10165b3846
chenxuhua <chenxvhua@mobanker.com>
2017-12-29 13:54:16 +0800

    feat(generator-test):完成第三阶段内容

--- app/templates/src/scripts/build/index.js
+++ app/templates/src/scripts/build/index.js
@@ -14,12 +14,12 @@ function wrap(func) {
     //包装函数返回的新函数在执行时，将会返回一个Promise对象，
     return function () {
         return new Promise((resolve, reject) => {
-            arguments[arguments.length++] = function (err, ...rest) {
+            arguments[arguments.length++] = function (err, result) {
                 if (err) {
                     reject(err);
                 }
                 //异步回掉进入的时候将异步结果resolve回去
-                resolve(rest);
+                resolve(result);
             }
             //此处可以看出包装函数在执行时实际上还是执行原来的异步函数func,只是对arguments做了修改
             func.apply(this, arguments)
